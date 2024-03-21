a5cbfd49ad1d909c8b524b5d3f412dd807025f68
mteresacastro <m.teresacastrogonzalez@gmail.com>
2024-03-21 17:24:11 +0100

    add some coin

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,30 @@
 <script setup>
+import {ref} from 'vue'
+
+const monedas = ref([
+      { codigo: 'USD', texto: 'Dolar de Estados Unidos'},
+      { codigo: 'MXN', texto: 'Peso Mexicano'},
+      { codigo: 'EUR', texto: 'Euro'},
+      { codigo: 'GBP', texto: 'Libra Esterlina'},
+  ])
 
 </script>
 
 <template>
   <div class="contenedor">
     <h1 class="titulo">Cotizador de <span>Criptomonedas</span></h1>
+  </div>
+  <div class="contenido">
+    <form class="formulario">
+        <div class="campo">
+            <label for="moneda">Moneda:</label>
+            <select id="moneda">
+                
+            </select>
+
+        </div>
+    </form>
+
   </div>
 
 </template>
