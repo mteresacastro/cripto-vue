a8cec0ce1996f9b80a92ef8209c9ca405e26ee51
mteresacastro <m.teresacastrogonzalez@gmail.com>
2024-03-25 10:49:20 +0100

    validate form

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,40 @@
 <script setup>
-import {ref, onMounted} from 'vue'
+    import {ref, reactive, onMounted} from 'vue'
+    import Alerta from './components/Alerta.vue'
 
-const monedas = ref([
-      { codigo: 'USD', texto: 'Dolar de Estados Unidos'},
-      { codigo: 'MXN', texto: 'Peso Mexicano'},
-      { codigo: 'EUR', texto: 'Euro'},
-      { codigo: 'GBP', texto: 'Libra Esterlina'},
-  ])
+    const monedas = ref([
+        { codigo: 'USD', texto: 'Dolar de Estados Unidos'},
+        { codigo: 'MXN', texto: 'Peso Mexicano'},
+        { codigo: 'EUR', texto: 'Euro'},
+        { codigo: 'GBP', texto: 'Libra Esterlina'},
+    ])
 
-const cryptomonedas = ref([])
+    const criptomonedas = ref([])
+    const error = ref('')
 
-onMounted(() =>{
-    const url = 'https://min-api.cryptocompare.com/data/top/mktcapfull?limit=20&tsym=USD'
-    fetch(url)
-        .then(respuesta => respuesta.json())
-        .then(({Data}) =>{
-            cryptomonedas.value = Data
-        })
-})
+    const cotizar = reactive({
+        moneda:'',
+        criptomoneda:''
+    })
+
+    onMounted(() =>{
+        const url = 'https://min-api.cryptocompare.com/data/top/mktcapfull?limit=20&tsym=USD'
+        fetch(url)
+            .then(respuesta => respuesta.json())
+            .then(({Data}) =>{
+                criptomonedas.value = Data
+            })
+    })
+
+    const cotizarCripto = () => {
+        //Validar que cotizar esta lleno
+       if(Object.values(cotizar).includes('')) {
+        error.value= 'Todos los campos son obligatorios'
+        return
+       }
+       error.value = ''
+       console.log('cotizando...')
+    }
 
 </script>
 
@@ -26,10 +43,19 @@ onMounted(() =>{
     <h1 class="titulo">Cotizador de <span>Criptomonedas</span></h1>
   
     <div class="contenido">
-        <form class="formulario">
+        <Alerta
+            v-if="error"
+        >{{ error }}</Alerta>
+        <form 
+            class="formulario"
+            @submit.prevent="cotizarCripto"
+        >
             <div class="campo">
                 <label for="moneda">Moneda:</label>
-                <select id="moneda">
+                <select 
+                    id="moneda"
+                    v-model="cotizar.moneda"
+                >
                     <option value="">-- Selecciona --</option>
                     <option 
                         v-for="moneda in monedas" 
@@ -38,13 +64,16 @@ onMounted(() =>{
                 </select>
             </div>
             <div class="campo">
-                <label for="crypto">Crypomoneda:</label>
-                <select id="crypto">
+                <label for="cripto">Cripomoneda:</label>
+                <select 
+                    id="cripto"
+                    v-model="cotizar.criptomoneda"
+                >
                     <option value="">-- Selecciona --</option>
                     <option 
-                        v-for="cryptomoneda in cryptomonedas" 
-                        :value="cryptomoneda.CoinInfo.Name"
-                    >{{ cryptomoneda.CoinInfo.FullName }}</option>
+                        v-for="criptomoneda in criptomonedas" 
+                        :value="criptomoneda.CoinInfo.Name"
+                    >{{ criptomoneda.CoinInfo.FullName }}</option>
                 </select>
                 <input type="submit" value="Cotizar" />
             </div>
